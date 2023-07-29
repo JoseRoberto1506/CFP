0c8b9490e4921b4edc11ceeabd75674b1a76ff35
Indrath <vinicius.barboza@ufrpe.br>
2023-07-29 18:15:09 -0300

    Filtrar colunas, equilibrar dataset e mudar para inteiros

--- pages/4_Agrupamento.py
+++ pages/4_Agrupamento.py
@@ -21,3 +21,21 @@ def header():
                 Esta página apresenta algumas opções de filtragem a partir do <i>dataset</i>.
                 """,
                 unsafe_allow_html= True)
+
+ds = pd.read_csv("./data/telco_churn_data.csv")
+
+#Limpeza dos Dados
+
+ds = ds.drop('Customer ID', axis = 1)
+
+filtro = ds['Churn Value'] == 0
+linhas_a_excluir = ds[filtro].sample(n=3305, random_state=42)
+df_filtrado = ds.drop(linhas_a_excluir.index) 
+
+
+#Transformação dos Dados
+
+df_filtrado2 = df_filtrado
+df_filtrado2['Offer'] = df_filtrado2['Offer'].replace({'Offer A': 1, 'Offer B': 2, 'Offer C': 3, 'Offer D': 4, 'Offer E': 5})
+df_filtrado3 = df_filtrado2
+df_filtrado3['Offer'] = df_filtrado3['Offer'].fillna(0)
