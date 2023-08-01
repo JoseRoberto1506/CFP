96f8bd6def66cb2e5f343a9c5a6ba3a0f0c73ae6
José Roberto Araújo <84887543+JoseRoberto1506@users.noreply.github.com>
2023-08-01 12:25:41 -0300

    Organização do código da página de agrupamento

--- pages/4_Agrupamento.py
+++ pages/4_Agrupamento.py
@@ -19,6 +19,14 @@ st.set_page_config(
     }
 )
 
+
+def main():
+    header()
+    df_processado = preprocessamento()
+    df_transformado = transformacao(df_processado)
+    clusterizacao(df_transformado)
+
+
 def header():
     st.header("Agrupamento de Dados")
     st.markdown("""
@@ -26,142 +34,115 @@ def header():
                 """,
                 unsafe_allow_html= True)
 
-ds = pd.read_csv("./data/telco_churn_data.csv")
-dsmutavel = ds.copy()
 
-#Limpeza dos Dados
+def preprocessamento():
+    st.markdown("### Pré-processamento")
+    df = ler_dataset()
+    df = df.drop(['Customer ID'], axis = 1)
+    linhas_a_excluir = df[df['Churn Value'] == 0].sample(n=3305, random_state=42)
+    df = df.drop(linhas_a_excluir.index)
 
-dsmutavel = dsmutavel.drop('Customer ID', axis = 1)
-
-dsmutavel = dsmutavel.drop('City', axis = 1)
-
-dsmutavel = dsmutavel.drop('Latitude', axis = 1)
-
-dsmutavel = dsmutavel.drop('Longitude', axis = 1)
-
-dsmutavel = dsmutavel.drop('Zip Code', axis = 1)
-
-
-filtro = dsmutavel['Churn Value'] == 0
-linhas_a_excluir = dsmutavel[filtro].sample(n=3305, random_state=42)
-dsmutavel = dsmutavel.drop(linhas_a_excluir.index) 
-
-#Transformação dos Dados
-
-def transformar_para_inteiro (dataset_recebido2, coluna):
-    dataframe=dataset_recebido2.copy()
-    lista_valores_distintos = dataframe[coluna].unique().tolist()
-    lista_valores_distintos=[vd for vd in lista_valores_distintos if pd.notna(vd)]
-    eh_booleano = 'Yes' in lista_valores_distintos
-    if eh_booleano:
-        dataframe[coluna] = dataframe[coluna].replace({'No': 0, 'Yes': 1})
-    else:
-        if dataframe[coluna].isnull().any():
-            dataframe[coluna]=dataframe[coluna].fillna(0)
-        for i, valor in enumerate (lista_valores_distintos):
-            dataframe[coluna] = dataframe[coluna].replace({valor: i+1})
-    return dataframe
-
-def para_inteiro_varias_colunas (dataset_recebido, lista_colunas):
-    dataframe=dataset_recebido.copy()
-    for i, valor in enumerate (lista_colunas):
-        dataframe=transformar_para_inteiro(dataframe, valor)
-    return dataframe
-
-
-lista_nomes_colunas=['Offer', 'Internet Type', 'Contract', 'Payment Method', 'Gender',
-                                                  'Married', 'Referred a Friend', 'Phone Service', 'Multiple Lines',
-                                                    'Internet Service', 'Online Security', 'Online Backup',
-                                                      'Device Protection Plan', 'Premium Tech Support', 'Streaming TV',
-                                                        'Streaming Movies', 'Streaming Music', 'Unlimited Data', 
-                                                        'Paperless Billing', 'Under 30', 'Senior Citizen', 'Married', 'Dependents',
-                                                        'Churn Reason', 'Churn Category']
-dsmutavel=para_inteiro_varias_colunas(dsmutavel, lista_nomes_colunas)
-
-dsmutavel['Customer Satisfaction']=dsmutavel['Customer Satisfaction'].fillna(0)
-
+    if st.checkbox("Mostrar dataset após o pré-processamento dos dados"):
+        st.dataframe(df)
+    st.divider()
+    
+    return df
 
 
 @st.cache_data
 def ler_dataset():
-    return dsmutavel
-
-def filters_section():
-    st.markdown("#### Filtros")
-    df = ler_dataset()
-    filtros_padrao = df.columns.to_list()
-    filtros = st.multiselect(
-        'Filtrar por:', 
-        df.columns, 
-        filtros_padrao[1:10]
-        )
-    ordernar_por = st.multiselect(
-        'Ordenar por:', 
-        filtros, 
-        filtros[0]
-        )
-    st.dataframe(df.filter(filtros).sort_values(ordernar_por))
-
-def header2():
-    st.header("Método do Cotovelo")
+    df = pd.read_csv("./data/telco_churn_data.csv")
+    return df.copy()
 
 
-def cotovelo():
+def transformacao(df):
+    st.markdown("### Transformação")
+    lista_nomes_colunas = ['Offer', 'Internet Type', 'Contract', 'Payment Method', 'Gender', 'Married', 
+                            'Referred a Friend', 'Phone Service', 'Multiple Lines', 'Internet Service', 
+                            'Online Security', 'Online Backup', 'Device Protection Plan', 'Premium Tech Support', 
+                            'Streaming TV', 'Streaming Movies', 'Streaming Music', 'Unlimited Data', 'Paperless Billing', 
+                            'Under 30', 'Senior Citizen', 'Married', 'Dependents', 'City', 'Churn Reason', 'Churn Category']
+    dataframe = para_inteiro_varias_colunas(df, lista_nomes_colunas)
+    dataframe['Customer Satisfaction'].fillna(0, inplace=True)
+    
+    if st.checkbox("Mostrar dataset após a transformação dos dados"):
+        st.dataframe(dataframe)
+    st.divider()
+
+    return dataframe
+
+
+def para_inteiro_varias_colunas (dataset_recebido, lista_colunas):
+    dataframe = dataset_recebido
+    for coluna in lista_colunas:
+        dataframe = transformar_para_inteiro(dataframe, coluna)
+
+    return dataframe
+
+
+def transformar_para_inteiro (dataset_recebido, coluna):
+    dataframe = dataset_recebido
+    lista_valores_distintos = dataframe[coluna].unique().tolist()
+    lista_valores_distintos = [vd for vd in lista_valores_distintos if pd.notna(vd)]
+    if 'Yes' in lista_valores_distintos:
+        dataframe[coluna] = dataframe[coluna].replace({'No': 0, 'Yes': 1})
+    else:
+        if dataframe[coluna].isnull().any():
+            dataframe[coluna].fillna(0, inplace=True)
+        for i, valor in enumerate(lista_valores_distintos):
+            dataframe[coluna].replace({valor: i+1}, inplace=True)
+
+    return dataframe
+
+
+def clusterizacao(df):
+    st.markdown("### Clusterização")
+    with st.expander("Método do Cotovelo para a identificação da quantidade ótima de clusters"):
+        cotovelo(df)
+    with st.expander("Método da Silhueta para a identificação da quantidade ótima de clusters"):
+        silhueta(df)
+
+
+def cotovelo(df):
+    st.markdown("### Método do Cotovelo")
     inertia_values = []
-
-    k_values = range(1, 11)
-
+    k_values = range(2, 31)
     for k in k_values:
         kmeans = KMeans(n_clusters=k, random_state=42)
-        kmeans.fit(dsmutavel)
+        kmeans.fit(df)
         inertia_values.append(kmeans.inertia_)
 
+    plt.figure(figsize=(12, 10))
     plt.plot(k_values, inertia_values, marker='o')
     plt.xlabel('Número de clusters (K)')
     plt.ylabel('Inércia')
     plt.title('Método do Cotovelo para K-means')
     plt.xticks(k_values)
-    plt.show()
     st.pyplot(plt)
 
-def header3():
-    st.header("Análise da Silhueta")
 
-
-def silhueta():
-    k_values = range(2, 11)
-
-    plt.figure()
-    plt.close()
-
+def silhueta(df):
+    st.markdown("### Análise da Silhueta")
+    k_values = range(2, 21)
     selected_k = st.selectbox('Escolha o valor de K:', k_values)
-
-    
     kmeans = KMeans(n_clusters=selected_k, random_state=42)
-    cluster_labels = kmeans.fit_predict(dsmutavel)
-    
-    silhouette_avg = silhouette_score(dsmutavel, cluster_labels)
 
-    silhouette_values = silhouette_samples(dsmutavel, cluster_labels)
-    
-    plt.figure(figsize=(6, 4))
-    
+    cluster_labels = kmeans.fit_predict(df)
+    silhouette_avg = silhouette_score(df, cluster_labels)
+    silhouette_values = silhouette_samples(df, cluster_labels)   
+     
+    plt.figure(figsize=(12, 10))    
     y_lower = 10
     for i in range(selected_k):
         cluster_silhouette_values = silhouette_values[cluster_labels == i]
         cluster_silhouette_values.sort()
-        
         size_cluster_i = cluster_silhouette_values.shape[0]
         y_upper = y_lower + size_cluster_i
-        
         plt.fill_betweenx(np.arange(y_lower, y_upper), 0, cluster_silhouette_values, alpha=0.7)
         plt.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
-            
         y_lower = y_upper + 10
     
-
     plt.axvline(x=silhouette_avg, color='red', linestyle='--', label='Média', linewidth=2)
-    
     plt.xlabel('Valores do coeficiente de silhueta')
     plt.ylabel('Cluster')
     plt.title('Análise das Silhuetas para K = {}'.format(selected_k))
@@ -171,56 +152,51 @@ def silhueta():
     plt.legend()
     st.pyplot(plt)
 
-def header4():
-    st.header("Clusterização")
 
-def clusterizacao():
-    colunas_numericas = dsmutavel.select_dtypes(include=[np.number]).columns.tolist()
+def clusterizacao1(df):
+    st.markdown("### Clusterização")
+    colunas_numericas = df.select_dtypes(include=[np.number]).columns.tolist()
 
-# Caixas de seleção para escolher as duas colunas para clusterização
+    # Caixas de seleção para escolher as duas colunas para clusterização
     coluna_x = st.selectbox('Selecione a primeira coluna:', colunas_numericas)
     coluna_y = st.selectbox('Selecione a segunda coluna:', colunas_numericas)
 
-# Criar DataFrame apenas com as colunas selecionadas
-    data_selected = dsmutavel[[coluna_x, coluna_y]]
+    # Criar DataFrame apenas com as colunas selecionadas
+    data_selected = df[[coluna_x, coluna_y]]
 
-# Aplicar o algoritmo KMeans aos dados
+    # Aplicar o algoritmo KMeans aos dados
     kmeans = KMeans(n_clusters=3)
     kmeans.fit(data_selected)
 
-# Obtendo as etiquetas dos pontos (clusters)
+    # Obtendo as etiquetas dos pontos (clusters)
     labels = kmeans.labels_
 
-# Adicionando as etiquetas dos clusters ao DataFrame
+    # Adicionando as etiquetas dos clusters ao DataFrame
     data_selected['Cluster'] = labels
 
-# Plotando os dados coloridos de acordo com os clusters
+    # Plotando os dados coloridos de acordo com os clusters
     plt.figure(figsize=(10, 6))
     plt.scatter(data_selected[coluna_x], data_selected[coluna_y], c=labels, cmap='viridis', alpha=0.6, s=100, label='Clusters')
 
-# Plotando todos os dados do DataFrame em cinza para visualizar a dispersão completa
-    plt.scatter(dsmutavel[coluna_x], dsmutavel[coluna_y], c='gray', alpha=0.4, s=20, label='Dados Originais')
+    # Plotando todos os dados do DataFrame em cinza para visualizar a dispersão completa
+    plt.scatter(df[coluna_x], df[coluna_y], c='gray', alpha=0.4, s=20, label='Dados Originais')
 
     plt.xlabel(coluna_x)
     plt.ylabel(coluna_y)
     plt.title('Clusterização do DataFrame dsmutavel usando K-Means')
     plt.colorbar(label='Clusters')
     plt.legend()
-    plt.show()
 
-# Exibindo o plot no Streamlit
-    st.pyplot()
+    # Exibindo o plot no Streamlit
+    st.pyplot(plt)
 
-def header4():
-    st.header("Clusterização")
-
-def clusterizacao():
-    colunas_numericas = dsmutavel.select_dtypes(include=[np.number]).columns.tolist()
+def clusterizacao2(df):
+    colunas_numericas = df.select_dtypes(include=[np.number]).columns.tolist()
 
     coluna_x = st.selectbox('Selecione a primeira coluna:', colunas_numericas)
     coluna_y = st.selectbox('Selecione a segunda coluna:', colunas_numericas)
 
-    data_selected = dsmutavel[[coluna_x, coluna_y]]
+    data_selected = df[[coluna_x, coluna_y]]
 
     kmeans = KMeans(n_clusters=3)
     kmeans.fit(data_selected)
@@ -232,27 +208,14 @@ def clusterizacao():
     plt.figure(figsize=(10, 6))
     plt.scatter(data_selected[coluna_x], data_selected[coluna_y], c=labels, cmap='viridis', alpha=0.6, s=100, label='Clusters')
 
-    plt.scatter(dsmutavel[coluna_x], dsmutavel[coluna_y], c='gray', alpha=0.4, s=20, label='Dados Originais')
+    plt.scatter(df[coluna_x], df[coluna_y], c='gray', alpha=0.4, s=20, label='Dados Originais')
 
     plt.xlabel(coluna_x)
     plt.ylabel(coluna_y)
     plt.title('Clusterização do DataFrame dsmutavel usando K-Means')
     plt.colorbar(label='Clusters')
     plt.legend()
-    plt.show()
 
-    st.pyplot()
+    st.pyplot(plt)
 
-
-header()
-filters_section()
-header2()
-cotovelo()
-header3()
-silhueta()
-header4()
-clusterizacao()
-
-
-header4()
-clusterizacao()
+main()
